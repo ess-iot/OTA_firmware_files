c8f64c10d11758d02cf7ea17133261bb7c7bd86e
ess-iot <essitco.ecc@gmail.com>
2024-12-12 16:44:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,3 @@
-import network
-import urequests
-import machine
-import time
-import ota_update
-# Main Function
-def main():
-    # Check for OTA updates
-    ota_update.ota_update()
+print("hello main.py\n")
+print("NEW main\n")
 
-    # Run main application (LED blinking)
-    print("hello main.py\n")
-    print("NEW main\n")
-
-main()
