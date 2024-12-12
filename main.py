3c49feb2d7ae6c126f309dbb2701cf8aa395c926
ess-iot <essitco.ecc@gmail.com>
2024-12-12 13:46:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+import network
+import urequests
+import machine
+import time
+import ota_update
+# Main Function
+def main():
+    # Check for OTA updates
+    ota_update.ota_update()
+
+    # Run main application (LED blinking)
+    print("hello main.py\n")
+
+main()
