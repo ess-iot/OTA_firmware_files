ae32675f0ddf6804fb35eff1ce18f09ea02d55ee
ess-iot <essitco.ecc@gmail.com>
2024-12-13 10:33:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print("hello main.py\n")
-print("NEW main................\n")
-print("firmware update successfully\n")
-
+import machine
+import utime
+ 
+led = machine.Pin('LED', machine.Pin.OUT)
+while True:
+    led.value(1)
+    utime.sleep(1)
+    led.value(0)
+    utime.sleep(0.4)
