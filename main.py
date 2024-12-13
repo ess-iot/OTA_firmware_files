56de0af36efeb7105258c7a2396edc19ce0e68ed
ess-iot <essitco.ecc@gmail.com>
2024-12-13 13:30:42 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import machine
 import time
+import ota
+import update_firmware
 
 # Configure the LED pin (built-in LED is on GPIO 25)
 led = machine.Pin('LED', machine.Pin.OUT)
@@ -8,4 +10,9 @@ led = machine.Pin('LED', machine.Pin.OUT)
 while True:
     led.toggle()  # Toggle the LED state
     time.sleep(0.6)  # Wait for 1 second
+    if ota.check_for_updates():
+        print("Update available!")
+        update_firmware.auto_update()
+    else:
+        print("Not available.")
 
